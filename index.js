857a94b5982f49c2eca7598977a6e44b8b8e8c6f
Nicolas Fantone <nfantone@contentamobile.com>
2015-09-04 17:31:46 -0300

    Update eslint and linting config

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-'use strict';
-
 import DefaultRegistry from 'undertaker-registry';
 import GulpServer from './lib/serve';
 import ServeConfig from './lib/config';
 
 class GulpServeRegistry extends DefaultRegistry {
-	constructor(options) {
-		super();
-		this.config = new ServeConfig(options);
-	}
+  constructor(options) {
+    super();
+    this.config = new ServeConfig(options);
+  }
 
-	init(taker) {
-		let server = new GulpServer(this.config);
-		let recipes = {
-			serve: taker.series(function start(cb) {
-				server.serve(cb);
-			})
-		};
-		taker.task('serve', recipes.serve);
-	}
+  init(taker) {
+    let server = new GulpServer(this.config);
+    let recipes = {
+      serve: taker.series(function start(cb) {
+        server.serve(cb);
+      })
+    };
+    taker.task('serve', recipes.serve);
+  }
 }
 
 export default GulpServeRegistry;
